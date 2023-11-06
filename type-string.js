718e5540574aa8cd25577b3561c51f8c9250ec9f
Renato Home <remanero@gmail.com>
2023-11-06 20:38:20 -0300

    commit realizado em 06-11-2023 23:38

--- type-string.js
+++ type-string.js
@@ -1,3 +1,11 @@
 //alguma coisa de cadeias de caracteres
 
 const texto1 = "testando as strings ...."
+
+const nome = "Renato";
+
+const sobreNome = "Mendes"
+
+const nomeCompleto = nome + sobreNome;
+
+console.log(nomeCompleto);
