6f4a267f91264a5d7576fb0ebb5d32539760dc57
Renato Home <remanero@gmail.com>
2023-11-07 20:39:21 -0300

    var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,10 @@
+const numero = 123;
+const numeroString = "_123";
+
+console.log(numero === numeroString);
+
+console.log(numero == numeroString);
+
+console.log(numero + numeroString);
+
+console.log(numero +Number(numeroString));
---
+++ teste_variaveis.js
@@ -0,0 +1,22 @@
+
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area);
+// var area; //pode declarar depois e usar antes;)
+
+let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+let area;
+
+if (forma === 'retangulo') {
+area = altura*comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
+
